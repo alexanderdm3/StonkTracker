cf9769fac185b6b3b18b84239b4491b597f5ab9e
Devin Alexander <dalexander1@costar.com>
2021-03-05 14:17:41 -0500

    Initial Commit

---
+++ src/StonkTracker/Business/Http/HttpHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StonkTracker.Business.Http
+{
+    public class HttpHelper
+    {
+    }
+}
